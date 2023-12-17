03b42fcd9e2d32ca9187559461f71a923f942617
FkLalita <faruq2142@gmail.com>
2023-12-17 04:21:37 +0100

    Added rust day 2

--- Day2/rustimpl/src/main.rs
+++ Day2/rustimpl/src/main.rs
@@ -1,13 +1,14 @@
 use std::fs::read_to_string;
-//use std::fs::File;
+
 fn main() {
     println!("Hello, world!");
     cube_count()
 }
 
 fn cube_count() {
-    let inputs = read_to_string("src/input.txt").expect("error reading files");
+    let inputs = read_to_string("input.txt").expect("error reading files");
     let mut sum_id = 0;
+    let mut totalsum = 0;
 
     for input in inputs.lines() {
         let (info, game) = input.split_once(": ").unwrap();
@@ -16,25 +17,42 @@ fn cube_count() {
         let mut is_possible = true;
 
         let game = game.split("; ").collect::<Vec<_>>();
+        let mut redsumV: Vec<i32> = Vec::new();
+        let mut bluesumV: Vec<i32> = Vec::new();
+        let mut greensumV: Vec<i32> = Vec::new();
+
         for g in game {
             let turns = g.split(", ").collect::<Vec<_>>();
             for t in turns {
                 let (cube, color) = t.split_once(" ").unwrap();
                 let cube: i32 = cube.parse().unwrap();
 
-                if color == "red" && cube > 12 {
-                    is_possible = false
-                } else if color == "blue" && cube > 14 {
-                    is_possible = false
-                } else if color == "green" && cube > 13 {
-                    is_possible = false
+                if color == "red" {
+                    redsumV.push(cube);
+                } else if color == "blue" {
+                    bluesumV.push(cube);
+                } else if color == "green" {
+                    greensumV.push(cube);
                 }
             }
+
+            let redsum = *redsumV.iter().max().unwrap_or(&0);
+            let bluesum = *bluesumV.iter().max().unwrap_or(&0);
+            let greensum = *greensumV.iter().max().unwrap_or(&0);
+
+            if redsum > 12 || bluesum > 14 || greensum > 13 {
+                is_possible = false;
+            }
+
+            totalsum += redsum * greensum * bluesum;
         }
 
         if is_possible {
             sum_id += info
         }
     }
-    println!("{}", sum_id)
+    println!(
+        "The sum of the possible game IDs is {} and the sum of the power of the minimum sets is {}",
+        sum_id, totalsum
+    )
 }
