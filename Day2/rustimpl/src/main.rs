a5e7c5c238b0083327a0b4c13a283bbde24549be
FkLalita <faruq2142@gmail.com>
2023-12-18 03:28:12 +0100

    done part with rust

--- Day2/rustimpl/src/main.rs
+++ Day2/rustimpl/src/main.rs
@@ -2,7 +2,7 @@ use std::fs::read_to_string;
 
 fn main() {
     println!("Hello, world!");
-    cube_count()
+    cube_count();
 }
 
 fn cube_count() {
@@ -10,49 +10,52 @@ fn cube_count() {
     let mut sum_id = 0;
     let mut totalsum = 0;
 
+    // Initialize maximum counts to the minimum possible value
+
     for input in inputs.lines() {
+        let mut red_max = 0;
+        let mut blue_max = 0;
+        let mut green_max = 0;
         let (info, game) = input.split_once(": ").unwrap();
         let (_, info) = info.split_once(" ").unwrap();
         let info: i32 = info.parse().unwrap();
         let mut is_possible = true;
 
         let game = game.split("; ").collect::<Vec<_>>();
-        let mut redsumV: Vec<i32> = Vec::new();
-        let mut bluesumV: Vec<i32> = Vec::new();
-        let mut greensumV: Vec<i32> = Vec::new();
-
         for g in game {
             let turns = g.split(", ").collect::<Vec<_>>();
             for t in turns {
-                let (cube, color) = t.split_once(" ").unwrap();
-                let cube: i32 = cube.parse().unwrap();
+                let (_, color) = t.split_once(" ").unwrap();
 
-                if color == "red" {
-                    redsumV.push(cube);
-                } else if color == "blue" {
-                    bluesumV.push(cube);
-                } else if color == "green" {
-                    greensumV.push(cube);
+                // Update maximum counts
+                let cube: i32 = t.split_once(" ").unwrap().0.parse().unwrap();
+                match color {
+                    "red" => red_max = red_max.max(cube),
+                    "blue" => blue_max = blue_max.max(cube),
+                    "green" => green_max = green_max.max(cube),
+                    _ => (),
+                }
+
+                // Check if the current cube count violates the conditions
+                if (color == "red" && cube > 12)
+                    || (color == "blue" && cube > 14)
+                    || (color == "green" && cube > 13)
+                {
+                    is_possible = false;
                 }
             }
-
-            let redsum = *redsumV.iter().max().unwrap_or(&0);
-            let bluesum = *bluesumV.iter().max().unwrap_or(&0);
-            let greensum = *greensumV.iter().max().unwrap_or(&0);
-
-            if redsum > 12 || bluesum > 14 || greensum > 13 {
-                is_possible = false;
-            }
-
-            totalsum += redsum * greensum * bluesum;
         }
+        totalsum += red_max * green_max * blue_max;
 
         if is_possible {
-            sum_id += info
+            // Calculate the product of maximum counts for each color
+
+            sum_id += info;
         }
     }
+
     println!(
-        "The sum of the possible game IDs is {} and the sum of the power of the minimum sets is {}",
+        "The sum of the possible game IDs is {}.\nThe sum of the powers of the minimum sets is {}.",
         sum_id, totalsum
-    )
+    );
 }
